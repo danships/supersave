9f8140eed63b7f8d41ed11281d32b65c52c1971b
Dan <git@debuggingdan.com>
2026-01-21 16:33:58 +0100

    refactor: add Migration type export (#30)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import SuperSave from './super-save.js';
 
 export { HookError } from './collection/error/index.js';
-export { Collection, Hooks, HttpContext } from './collection/types.js';
+export type { Collection, Hooks, HttpContext } from './collection/types.js';
 export { Query, Repository } from './database/entity-manager/index.js';
-export { BaseEntity, EntityDefinition } from './database/types.js';
+export type { BaseEntity, EntityDefinition } from './database/types.js';
+export type { Migration } from './super-save.js';
 
 export { SuperSave };
